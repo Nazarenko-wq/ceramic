dd238727e8f89a62e667ea9122c6d34cab50d8e7
Mihail Nazarenko <82104821+Nazarenko-wq@users.noreply.github.com>
2023-01-12 10:29:26 +0300

    feat: do filter block

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,32 +1,49 @@
 "use strict"
+// slider animation
+!(function(){
+    let sliderContent = document.querySelector('.slider_content');
+    let btnsBlock = document.querySelector('.btns_block');
+    let btns = document.querySelectorAll('.btn');
 
-let sliderContent = document.querySelector('.slider_content');
-let btnsBlock = document.querySelector('.btns_block');
-let btns = document.querySelectorAll('.btn');
+    btnsBlock.addEventListener('click', (e) => {
+        if(e.target.classList.contains('btn')) {
+            btns.forEach(elem => {
+                elem.classList.remove('active');
+            })
 
-btnsBlock.addEventListener('click', (e) => {
-    if(e.target.classList.contains('btn')) {
-        btns.forEach(elem => {
-            elem.classList.remove('active');
-        })
+            e.target.classList.add('active');
+            console.log(e.target);
+        }
 
-        e.target.classList.add('active');
-        console.log(e.target);
+        if(e.target.classList.contains('btn1')) {
+            sliderContent.style.left = '0px';
+        }
+
+        if(e.target.classList.contains('btn2')) {
+            sliderContent.style.left = '-1920px';
+        }
+
+        if(e.target.classList.contains('btn3')) {
+            sliderContent.style.left = '-3840px';
+        }
+
+        if(e.target.classList.contains('btn4')) {
+            sliderContent.style.left = '-5760px';
+        }
+    })
+})();
+
+// create brands block
+!(function(){
+    let brands = document.querySelector('.brands');
+
+    for(let i = 1; i < 10; i++) {
+        let item = document.createElement('div');
+        item.classList.add('item');
+        brands.append(item);
+
+        let logo = document.createElement('img');
+        logo.src = `./assets/images/img/brand_icon${i}.png`;
+        item.append(logo);
     }
-
-    if(e.target.classList.contains('btn1')) {
-        sliderContent.style.left = '0px';
-    }
-
-    if(e.target.classList.contains('btn2')) {
-        sliderContent.style.left = '-1920px';
-    }
-
-    if(e.target.classList.contains('btn3')) {
-        sliderContent.style.left = '-3840px';
-    }
-
-    if(e.target.classList.contains('btn4')) {
-        sliderContent.style.left = '-5760px';
-    }
-})
+})();
